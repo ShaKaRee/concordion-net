0b87fe63d9b3bfd62a97f6acacb0d838f8bd8ad2
ShaKaRee <concordion8@gmail.com>
2016-01-20 17:01:47 +0100

    improve performance and provide full OGNL support

--- Concordion.NET/Internal/SimpleEvaluator.cs
+++ Concordion.NET/Internal/SimpleEvaluator.cs
@@ -34,63 +34,6 @@ namespace Concordion.NET.Internal
 
         #region Methods
 
-        private void ValidateEvaluationExpression(string expression)
-        {
-            string METHOD_CALL_PARAMS = METHOD_NAME_PATTERN + " *\\( *" + RHS_VARIABLE_PATTERN + "(, *" + RHS_VARIABLE_PATTERN + " *)*\\)";
-            string METHOD_CALL_NO_PARAMS = METHOD_NAME_PATTERN + " *\\( *\\)";
-            string TERNARY_STRING_RESULT = " \\? " + STRING_PATTERN + " : " + STRING_PATTERN;
-            
-            List<string> regexPatterns = new List<string>();
-            regexPatterns.Add(PROPERTY_NAME_PATTERN);
-            regexPatterns.Add(METHOD_CALL_NO_PARAMS);
-            regexPatterns.Add(METHOD_CALL_PARAMS);
-            regexPatterns.Add(RHS_VARIABLE_PATTERN);
-            regexPatterns.Add(LHS_VARIABLE_PATTERN + "(\\." + PROPERTY_NAME_PATTERN +  ")+");
-            regexPatterns.Add(LHS_VARIABLE_PATTERN + " *= *" + PROPERTY_NAME_PATTERN);
-            regexPatterns.Add(LHS_VARIABLE_PATTERN + " *= *" + METHOD_CALL_NO_PARAMS);
-            regexPatterns.Add(LHS_VARIABLE_PATTERN + " *= *" + METHOD_CALL_PARAMS);
-            regexPatterns.Add(LHS_VARIABLE_PATTERN + TERNARY_STRING_RESULT);
-            regexPatterns.Add(PROPERTY_NAME_PATTERN + TERNARY_STRING_RESULT);
-            regexPatterns.Add(METHOD_CALL_NO_PARAMS + TERNARY_STRING_RESULT);
-            regexPatterns.Add(METHOD_CALL_PARAMS + TERNARY_STRING_RESULT);
-            regexPatterns.Add(LHS_VARIABLE_PATTERN + "\\." + METHOD_CALL_NO_PARAMS);
-            regexPatterns.Add(LHS_VARIABLE_PATTERN + "\\." + METHOD_CALL_PARAMS);
-            
-            expression = expression.Trim();
-
-            foreach (string regexPattern in regexPatterns) 
-            {
-                if (Regex.IsMatch(expression, regexPattern)) 
-                {
-                    return;
-                }
-            }
-
-            throw new InvalidOperationException("Invalid expression [" + expression + "]");
-        }
-
-        private void ValidateSetVariableExpression(string expression)
-        {
-            List<string> regexPatterns = new List<string>();
-            regexPatterns.Add(RHS_VARIABLE_PATTERN);
-            regexPatterns.Add(LHS_VARIABLE_PATTERN + "\\." + PROPERTY_NAME_PATTERN);
-            regexPatterns.Add(LHS_VARIABLE_PATTERN + " *= *" + PROPERTY_NAME_PATTERN);
-            regexPatterns.Add(LHS_VARIABLE_PATTERN + " *= *" + METHOD_NAME_PATTERN + " *\\( *\\)");
-            regexPatterns.Add(LHS_VARIABLE_PATTERN + " *= *" + METHOD_NAME_PATTERN + " *\\( *" + RHS_VARIABLE_PATTERN + "(, *" + RHS_VARIABLE_PATTERN + " *)*\\)");
-
-            expression = expression.Trim();
-
-            foreach (string regexPattern in regexPatterns)
-            {
-                if (Regex.IsMatch(expression, regexPattern))
-                {
-                    return;
-                }
-            }
-
-            throw new InvalidOperationException("Invalid expression [" + expression + "]");
-        }
-
         public static object ConvertToJavaTypes(object value)
         {
             if (value is Boolean) return new java.lang.Boolean((bool)value);
@@ -129,14 +72,12 @@ namespace Concordion.NET.Internal
 
         public override object  evaluate(string expression)
         {
-            this.ValidateEvaluationExpression(expression);
             var result = base.evaluate(expression);
             return ConvertToJavaTypes(result);
         }
 
         public override void setVariable(string expression, object value)
         {
-            this.ValidateSetVariableExpression(expression);
             base.setVariable(expression, ConvertToDotnetTypes(value));
         }
 
