eafac405e9cbba098877ea3b15273af53814eba6
ShaKaRee <concordion8@gmail.com>
2016-01-24 09:27:51 +0100

    simplify run test

--- Concordion.Spec/Concordion/Command/Run/RunTest.cs
+++ Concordion.Spec/Concordion/Command/Run/RunTest.cs
@@ -9,10 +9,7 @@ namespace Concordion.Spec.Concordion.Command.Run
     {
     	public string successOrFailure(string fragment, string hardCodedTestResult, string evaluationResult)
     	{
-    	    java.lang.System.setProperty("concordion.runner.runtestrunner",
-    	        typeof(RunTestRunner).AssemblyQualifiedName);
-
-    	    if ("SUCCESS".Equals(hardCodedTestResult))
+            if ("SUCCESS".Equals(hardCodedTestResult))
     	    {
     	        RunTestRunner.Result = Result.SUCCESS;
     	    }
